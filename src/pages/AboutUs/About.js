b5db1bfd067d8a4d4eabfd2f2568d43f72723325
adrianfinantyo <69502676+adrianfinantyo@users.noreply.github.com>
2021-12-09 11:33:22 +0700

    feat: fixing about us page

--- src/pages/AboutUs/About.js
+++ src/pages/AboutUs/About.js
@@ -1,21 +1,29 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ReactComponent as Spinner } from "images/loader.svg";
 import "styles/About.scss";
+import profileAdrian from "images/adrian_profile.png";
+import profileFernando from "images/fernando_profile.jpg";
+import profileLifosmin from "images/lifosmin_profile.jpg";
 
 const teamMember = [
   {
     name: "Adrian Finantyo",
     nim: "00000042580",
-    img: "https://adrianfinantyo.github.io/img/Its%20me.png",
+    img: profileAdrian,
+    summary:
+      "Hello! My name is Bonifasius Ariesto Adrian Finantyo, student of Universitas Multimedia Nusantara, Indonesia. An enthusiastic software engineer learns a new skill every day. Student, freelance web developer, and React JS enthusiast. Currently working on several web development projects. Active on organization and likes a challenge. Interested and still learning about web app development. Looking to leverage my knowledge and experience by becoming a laboratory assistant.",
   },
   {
     name: "Fernando Khorasani",
     nim: "000000XXXXX",
-    img: "https://adrianfinantyo.github.io/img/Its%20me.png",
+    img: profileFernando,
+    summary: "",
   },
   {
     name: "Lifosmin Simon",
     nim: "000000XXXXX",
-    img: "https://adrianfinantyo.github.io/img/Its%20me.png",
+    img: profileLifosmin,
+    summary: "",
   },
 ];
 
@@ -23,10 +31,10 @@ const Card = (props) => {
   return (
     <div className="card" onClick={() => props.setSide(props)}>
       <div className="card__wrapper">
-        <img src={props.img} alt={`${props.img} profile`} />
+        <img src={props.img} alt={`${props.img} profile`} loading="lazy" />
       </div>
       <div className="desc">
-        <p className="desc__title">Name</p>
+        {/* <p className="desc__title">Name</p> */}
         <p>{props.name}</p>
       </div>
     </div>
@@ -35,12 +43,27 @@ const Card = (props) => {
 
 const About = () => {
   const [side, setSide] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (side !== null) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        setLoading(true);
+      }, 500);
+    }
+  }, [side]);
 
   const freezeBody = (state) => {
     const body = document.querySelector("body");
     state ? body.classList.add("locked") : body.classList.remove("locked");
   };
+
   side !== null ? freezeBody(true) : freezeBody(false);
+
   return (
     <section className="section-separators">
       <div
@@ -48,7 +71,27 @@ const About = () => {
         onClick={() => setSide(null)}
       >
         <aside className={`desc-sidebar ${side !== null ? "show" : ""}`}>
-          <h1>test</h1>
+          <div>
+            {loading ? (
+              <div className="desc-sidebar__spinner">
+                <Spinner />
+              </div>
+            ) : (
+              <div className="desc-sidebar__container">
+                <div className="desc-sidebar__wrapper">
+                  <img
+                    src={side ? side.img : ""}
+                    alt={side ? side.name + " profiles" : ""}
+                  />
+                </div>
+                <div className="desc-sidebar__more">
+                  <h3 className="title">Summary</h3>
+                  <p className="summary"> {side ? side.summary : ""}</p>
+                  <h3 className="title">Contacts</h3>
+                </div>
+              </div>
+            )}
+          </div>
         </aside>
       </div>
       <h1>About Us</h1>
@@ -57,6 +100,7 @@ const About = () => {
           <Card
             setSide={setSide}
             name={data.name}
+            summary={data.summary}
             nim={data.nim}
             img={data.img}
             key={index}
